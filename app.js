3d1ddbbe4720bd29925d51f1b9891b4741befc8f
Jesús Rojas <jesusrpx9@gmail.com>
2023-11-02 06:54:42 -0400

    [ADD] Agregada vista home para usuario administrador

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // ===== IMPORTACIONES =====
 const express = require('express');
 const path = require('path');
+const login = require('./routes/login.js')
+
+// ===== RUTAS =====
 
 
 // ===== INSTANCIAS =====
@@ -11,12 +14,17 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/login', login);
+
 
 // ==== RUTAS ====
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/home', (req, res) => {
+  res.render('home');
+});
 
 // ==== SERVER ====
 app.listen(3000, () => {
---
+++ public/js/login.js
@@ -0,0 +1,71 @@
+let inputMail = document.querySelector('input[name="mail"]');
+let inputPassword = document.querySelector('input[name="password"]');
+let btnLogin = document.querySelector("#btnLogin");
+let formulario = document.querySelector('form');
+
+
+campos = {
+	mail: '',
+	password: '',
+}
+
+
+// ===== FUNCIONES =====
+
+function validarCampos(event) {
+	event.preventDefault();
+
+	if (event.target.value.trim() == '') {
+		campos[event.target.name] = '';
+		return
+	}
+
+	campos[event.target.name] = 'completado';
+}
+
+function comprobarEmail() {
+	console.log("Comprobando")
+}
+
+function mostrarAlerta(mensaje, nodo) {
+	let div = document.createElement('DIV');
+	let p = document.createElement('P');
+
+
+	while (nodo.firstChild) {
+		nodo.removeChild(nodo.firstChild);
+	}
+
+
+	div.classList.add('w-full', 'bg-orange-500', 'rounded-md', 'border', 'p-5', 'text-white')
+	p.textContent = mensaje;
+
+
+	div.appendChild(p);
+	nodo.appendChild(div);
+
+
+	setTimeout(()=>{
+		div.remove();
+	}, 4000);
+}
+
+
+// ===== EVENTOS =====
+
+formulario.addEventListener('submit', (event)=>{
+	event.preventDefault();
+
+	// Validar campos del formulario
+	if (Object.values(campos).includes('')) {
+		let box = document.querySelector('#boxMsg')
+		mostrarAlerta( 'Todos los campos deben ser llenados', box );
+		return;
+	}
+
+	// Enviar formulario
+	formulario.submit();
+})
+
+inputMail.addEventListener('input', validarCampos);
+inputPassword.addEventListener('input', validarCampos);
