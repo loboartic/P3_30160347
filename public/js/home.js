960d912a593090cded6b119a550ef45c1d231b06
Jesús Rojas <jesusrpx9@gmail.com>
2023-11-04 13:06:14 -0400

    [FIX] Correciones en la ventana modal

--- public/js/home.js
+++ public/js/home.js
@@ -1,41 +1,97 @@
 console.log("El script fue importado");
 
 // ====== SELECTORES =====
-const categorys = document.querySelector("#categorysLink");
+const categorys = document.querySelector("#categorys");
 const highlights = document.querySelector("#highlightLink");
 const btnCreateProduct = document.querySelector("#btnCreateProduct");
-const exitButton = document.querySelector("#exitButton");
-const label = document.querySelector('#inputLabel');
-const input = document.querySelector('#inputFile');
+const btnCategoris = document.querySelector("#btnCategoris");
+const exitButton = document.querySelector(".exitButton");
+const labelFile = document.querySelector('#inputLabel');
+const dropArea = document.querySelector('#dropArea');
 
+
+// ==== SECCIONES ====
+const seccionProductos = document.querySelector('#Productos');
+const seccionCategorys = document.querySelector('#Categorias')
 
 // ===== EVENTOS =====
 btnCreateProduct.addEventListener('click', openModal);
+btnCategoris.addEventListener('click', openModal)
 exitButton.addEventListener('click', closeModal);
-label.addEventListener('click', fileHandler);
+labelFile.addEventListener('click', fileHandler);
+dropArea.addEventListener('dragover', (event)=>{
+    event.preventDefault();
+    console.log("Hay un elemento encima");
+})
+dropArea.addEventListener('drop', (event)=>{
+    event.preventDefault();
+    console.log("Un elemento ha caido");
+    console.log(event.dataTransfer.files);
+
+    createPreviwCard(event.dataTransfer.files)
+})
+
+categorys.addEventListener('click', handlerSecction)
 
 // ===== FUNCIONES =====
 
 // Función para crear el producto
-function openModal(event) {
+function openModal( event ) {
     event.preventDefault();
     console.log("Se clickeo para aperturar el modal")
+
     const modal = document.querySelector("#modal");
+    const modalHeader = document.querySelector("#modal").children[0];
+    console.log(modal);
+    const modalContent = modal.children[0].children[1];
+    console.log(modalContent);
+
+    for (seccion of modalContent.children) {
+        seccion.getAttribute('data-modal') == event.target.parentElement.getAttribute('data-origin') ? seccion.classList.remove('hidden'):seccion.classList.add('hidden');
+    }
+
+    modalContent.classList.remove('hidden')
+    modalHeader.classList.remove('hidden')
     modal.classList.remove('hidden')
 }
 
-function closeModal(event) {
+function closeModal( event ) {
     event.preventDefault();
-    console.log("Se clickeo para cerrar el modal")
+
     const modal = document.querySelector("#modal");
+
+    event.target.parentElement.parentElement.parentElement.parentElement.classList.add('hidden');
     modal.classList.add('hidden')
 }
 
-function fileHandler(event) {
+function fileHandler( event ) {
+    const input = document.querySelector('#inputFile');
     input.click();
 }
 
 // Función que crea las tarjetas de los productos en el contenedor de productos
 function createProductTemplate( data ) {
 
+}
+
+function createPreviwCard( files ) {
+    const boxImgs = document.querySelector('#boxImgs');
+    boxImgs.classList.remove('hidden');
+
+    for (file of files) {
+        const box = document.createElement('div');
+        box.classList.add('border', 'rounded-lg', 'bg-blue-500')
+        box.textContent = file.name;
+
+        boxImgs.appendChild(box);
+    };
+}
+
+function handlerSecction( event ) {
+    if ( event.target.id === 'categorys') {
+        seccionProductos.classList.add('hidden');
+
+        seccionCategorys.classList.remove('hidden');
+        // seccionCategorys.classList.add('')
+    }
 }
